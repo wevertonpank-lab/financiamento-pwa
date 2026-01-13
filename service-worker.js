2b345d0517c33956c7241143257830e8772ad617
wevertonpank-lab <weverton.pank@gmail.com>
2026-01-13 20:26:32 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('financiamento-v1').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
